83f389e0b8671dd16b0ef136abc5772899e1df1a
alexbarksdale <ale.barksdale@gmail.com>
2020-06-06 03:12:33 -0500

    [Add] Renders content of a text file and prints to stdout

--- makesite.go
+++ makesite.go
@@ -2,22 +2,39 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"log"
+	"os"
 )
+
+type tmplData struct {
+	Content string
+}
 
 func readFile() string {
 	fileContents, err := ioutil.ReadFile("corpus/first-post.txt")
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to read file! %v", err)
 	}
 
 	return string(fileContents)
 }
 
-func renderTemplate() {}
+func renderTemplate() {
+	var tmplPath string = "/Users/alex/dev/backend/makesite/templates/template.tmpl"
+	contentValue := tmplData{readFile()}
+
+	tmpl, err := template.New("template.tmpl").ParseFiles(tmplPath)
+	if err != nil {
+		log.Fatalf("Failed to create a new template! %v", err)
+	}
+	if err := tmpl.Execute(os.Stdout, contentValue); err != nil {
+		log.Fatalf("Failed to execute the template! %v", err)
+	}
+}
 
 func main() {
-	fmt.Println("Make Site | A simple cli tool to generate a static site.")
-	output := readFile()
-	fmt.Println(output)
+	fmt.Printf("Make Site | A simple cli tool to generate a static site.")
+	renderTemplate()
 }
